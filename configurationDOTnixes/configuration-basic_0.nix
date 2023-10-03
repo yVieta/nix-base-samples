7211f9b5be995e11f0e21496f850edc8623dd550
Vieta <94648307+Quanterm@users.noreply.github.com>
2023-10-03 11:58:48 +0000

    Update configuration-basic_0.nix

--- configurationDOTnixes/configuration-basic_0.nix
+++ configurationDOTnixes/configuration-basic_0.nix
@@ -176,12 +176,22 @@ in
   #Container
   environment.etc."/containers/containers.conf".text = "";
   
-  # Automatic Garbage Collection
-  nix.gc = {
-   automatic = true;
-   dates = "weekly";
-   options = "--delete-older-than 7d";
-        };
+  
+  nix = { 
+   # Enable Flakes
+   #package = pkgs.nix;
+   settings = {
+        experimental-features = [ "nix-command" "flakes" ]; 
+        auto-optimise-store = true;
+   };
+   # Automatic Garbage Collection
+   gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 7d";
+   };
+ };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
